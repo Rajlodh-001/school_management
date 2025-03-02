787e2b7c125fa02517a7e81005a46d2da8d34cf3
Rajlodh-001 <Rajlodh911@gmail.com>
2025-03-02 21:05:58 +0530

    announcement part

---
+++ src/components/AttendanceChart.tsx
@@ -0,0 +1,89 @@
+"use client";
+import Image from "next/image";
+
+// import React, { PureComponent } from "react";
+import {
+  BarChart,
+  Bar,
+  Rectangle,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+const data = [
+  {
+    name: "Mon",
+    present: 40,
+    absent: 60,
+  },
+  {
+    name: "Tue",
+    present: 80,
+    absent: 75,
+  },
+  {
+    name: "Wed",
+    present: 86,
+    absent: 74,
+  },
+  {
+    name: "Thu",
+    present: 80,
+    absent: 40,
+  },
+  {
+    name: "Fri",
+    present: 90,
+    absent: 30,
+  },
+//   {
+//     name: "Sat",
+//     present: 86,
+//     absent: 66,
+//   },
+];
+
+const AttendanceChart = () => {
+  return (
+    <div className=" bg-white rounded-lg p-4 h-full">
+      <div className="flex justify-between items-center" >
+        <h1 className="text-lg font-semibold">Attendance</h1>
+        <Image src="/moreDark.png" alt="more-Image" width={20} height={20}/>
+      </div>
+      <ResponsiveContainer width="100%" height="90%">
+        <BarChart
+          width={500}
+          height={300}
+          data={data}
+          barSize={20}
+        >
+          <CartesianGrid strokeDasharray="3 3" vertical={false} stroke="#ddd"/>
+          <XAxis dataKey="name" axisLine={false} tick={{fill:"#d1d5db"}}  tickLine={false}/>
+          <YAxis  axisLine={false} tick={{fill:"#d1d5db"}}  tickLine={false}/>
+          <Tooltip  contentStyle={{borderRadius:"10px",borderColor:"lightgray" }}/>
+          <Legend align="left" verticalAlign="top" wrapperStyle={{paddingTop:"20px" ,paddingBottom:"40px"}}/>
+          <Bar
+            dataKey="present"
+            fill="#FAE27C"
+            // activeBar={<Rectangle fill="pink" stroke="blue" />}
+            legendType="circle"
+            radius={[10,10,0,0]}
+          />
+          <Bar
+            dataKey="absent"
+            fill="#C3EBFA"
+            // activeBar={<Rectangle fill="gold" stroke="purple" />}
+            legendType="circle"
+            radius={[10,10,0,0]}
+          />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+export default AttendanceChart;
