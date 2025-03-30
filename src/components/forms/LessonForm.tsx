fe1f308deee7cb616a4b034138e79d501f037114
Rajlodh-001 <Rajlodh911@gmail.com>
2025-03-30 22:20:08 +0530

    lessons Form added

---
+++ src/components/forms/LessonForm.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import InputField from "../InputField";
+import Image from "next/image";
+
+const schema = z.object({
+  subject: z.string().min(3, { message: "Subject Name is required !" }),
+  class: z.string().min(1, { message: "Class is required !" }),
+ 
+  teacher: z.string().min(3, { message: "Teacher's Name is required !" }),
+});
+
+type Inputs = z.infer<typeof schema>;
+
+const LessonForm = ({
+  type,
+  data,
+}: {
+  type: "create" | "update";
+  data?: any;
+}) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>({
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+  });
+
+  return (
+    <form action="" className="flex flex-col gap-4" onSubmit={onSubmit}>
+      <h1 className="text-xl font-semibold">
+        
+        {/* Create a new Class */}
+        {type === "create" ? "Create a new Lesson" : "Update this  Lesson"}
+        </h1>
+      <span className="text-xs text-gray-400 font-medium">
+        Authentic Information
+      </span>
+      <div className="flex justify-between flex-wrap gap-4">
+        <InputField
+          label="Subject Name"
+          name="subject"
+          defaultValue={data?.subject}
+          register={register}
+          error={errors?.subject}
+        />
+
+        <InputField
+          label="Class"
+          name="class"
+          defaultValue={data?.class}
+          register={register}
+          error={errors?.class}
+        />
+
+        <InputField
+          label="Teacher's Name"
+          name="teacher"
+          defaultValue={data?.teacher}
+          register={register}
+          error={errors?.teacher}
+        />
+
+
+      </div>
+
+      <button className="bg-blue-400 text-white p-2 rounded-md">
+        {type === "create" ? "Create" : "Update"}
+      </button>
+    </form>
+  );
+};
+
+export default LessonForm;
